f166aefae63f6284adcd7b000019cf990ec5417e
Mistralys <s.mordziol@gmail.com>
2025-12-14 16:56:08 +0100

    Added original file name case restoration.

--- bin/checkorder.php
+++ bin/checkorder.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Script used to guarantee that the `Plugins.txt` entries are
+ * sorted according to the reference file `Plugins-regular-stable.txt`.
+ */
 
 declare(strict_types=1);
 
@@ -10,10 +14,31 @@ require_once __DIR__.'/vendor/autoload.php';
 $referenceFile = FileInfo::factory(__DIR__.'/../Plugins-regular-stable.txt');
 $targetFile = FileInfo::factory(__DIR__.'/../Plugins.txt');
 
+echo str_repeat('-', 64).PHP_EOL;
+echo 'SAFEGUARDING PREVIOUS LOAD ORDER'.PHP_EOL;
+echo str_repeat('-', 64).PHP_EOL;
+echo PHP_EOL;
+echo '- Detecting plugin files...';
+
+$knownPlugins = FileHelper::createFileFinder('C:\Steam\steamapps\common\Starfield\Data')
+    ->includeExtensions(array('esm', 'esp'))
+    ->makeRecursive()
+    ->setPathmodeStrip()
+    ->getMatches();
+
+$lookup = array();
+foreach($knownPlugins as $fileName) {
+    $lookup[strtolower($fileName)] = $fileName;
+    echo '.';
+}
+
+echo 'OK'.PHP_EOL;
+echo '- Found ['.count($knownPlugins).'] plugins.'.PHP_EOL;
+
 $referenceLines = filterLines($referenceFile);
 $targetLines = filterLines($targetFile);
 
-echo 'Sorting '.$targetFile->getName()." according to ".$referenceFile->getName()."\n";
+echo '- Sorting ['.$targetFile->getName()."] according to [".$referenceFile->getName()."]".PHP_EOL;
 
 // sort target lines in order to keep the reference lines in the exact same order,
 // appending all new lines at the end of the file.
@@ -30,13 +55,25 @@ foreach ($referenceLines as $refLine) {
 
 // append all remaining target lines that were not in the reference file
 if (count($targetLines) > 0) {
-    echo 'Appending '.count($targetLines)." new lines at the end of the file.\n";
+    echo '- Appending ['.count($targetLines)."] new lines at the end of the file.\n";
     array_push($sortedTargetLines, ...$targetLines);
 }
 
+echo '- Adjusting file name case...';
+
+foreach($sortedTargetLines as $idx => $targetLine) {
+    $search = ltrim($targetLine, '*');
+    if(isset($lookup[$search])) {
+        $sortedTargetLines[$idx] = '*'.$lookup[$search];
+        echo '.';
+    }
+}
+
+echo 'OK'.PHP_EOL;
+
 $targetFile->putContents(implode("\r\n", $sortedTargetLines)."\r\n");
 
-echo 'Done.'."\n";
+echo PHP_EOL.'ALL DONE.'.PHP_EOL;
 
 function filterLines(FileInfo $file) : array
 {
